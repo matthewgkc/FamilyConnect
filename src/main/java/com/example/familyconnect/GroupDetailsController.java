5073a906280b35774db47fadf7cfa7711ba433fe
bearfaceington <georgioangelo@gmail.com>
2024-04-27 19:40:09 +1000

    added home button in create group page and group details page

--- src/main/java/com/example/familyconnect/GroupDetailsController.java
+++ src/main/java/com/example/familyconnect/GroupDetailsController.java
@@ -1,4 +1,23 @@
 package com.example.familyconnect;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class GroupDetailsController {
+    @FXML
+    private Button backHomeButton;
+
+    @FXML
+    protected void backtoHome() throws IOException {
+        Stage stage = (Stage) backHomeButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
+        stage.setScene(scene);
+    }
 }
+
