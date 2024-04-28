f93fbd661b1c8b06d41dbc28563d1a753d37f004
bearfaceington <georgioangelo@gmail.com>
2024-04-28 21:48:58 +1000

    merged individual overview page into master branch

---
+++ src/main/java/com/example/familyconnect/OverviewController.java
@@ -0,0 +1,17 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package com.example.familyconnect;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
+public class OverviewController {
+    @FXML
+    private ChoiceBox<String> trackingPeriodChoiceBox;
+
+    public OverviewController() {
+    }
+}
--- src/main/java/com/example/familyconnect/UserProfileController.java
+++ src/main/java/com/example/familyconnect/UserProfileController.java
@@ -1,70 +1,64 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package com.example.familyconnect;
 
+import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
-import java.io.IOException;
 
-/**
- * Controller for user-profile-view.fxml
- */
 public class UserProfileController {
-
-    /**
-     * Button to send user back to homepage
-     */
     @FXML
     private Button backHomeButton;
-
-    /**
-     * Button to send user to edit profile page
-     */
     @FXML
     private Button editProfileButton;
-
-    /**
-     * Button to delete user profile and bring back to login page (to-do)
-     */
     @FXML
     private Button deleteProfileButton;
+    @FXML
+    private Button individualOverviewButton;
 
-    /**
-     * Generates homepage scene
-     */
+    public UserProfileController() {
+    }
+
     @FXML
     protected void backtoHome() throws IOException {
-        Stage stage = (Stage) backHomeButton.getScene().getWindow();
+        Stage stage = (Stage)this.backHomeButton.getScene().getWindow();
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
+        Scene scene = new Scene((Parent)fxmlLoader.load(), 300.0, 450.0);
         stage.setScene(scene);
     }
 
-    /**
-     * Allows edit profile & delete profile buttons to be visible
-     */
     @FXML
-    protected void expandProfileOptions() {
-        if ((editProfileButton).isVisible() || (deleteProfileButton).isVisible()) {
-            (editProfileButton).setVisible(false);
-            (deleteProfileButton).setVisible(false);
-        }
-        else {
-            (editProfileButton).setVisible(true);
-            (deleteProfileButton).setVisible(true);
-        }
+    protected void onIndividualOverviewClick() throws IOException {
+        Stage stage = (Stage)this.individualOverviewButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("individual-overview.fxml"));
+        Scene scene = new Scene((Parent)fxmlLoader.load(), 300.0, 450.0);
+        stage.setScene(scene);
     }
 
-    /**
-     * Generates edit profile page
-     */
+    @FXML
+    protected void expandProfileOptions() throws IOException {
+        if (!this.editProfileButton.isVisible() && !this.deleteProfileButton.isVisible()) {
+            this.editProfileButton.setVisible(true);
+            this.deleteProfileButton.setVisible(true);
+        } else {
+            this.editProfileButton.setVisible(false);
+            this.deleteProfileButton.setVisible(false);
+        }
+
+    }
+
     @FXML
     protected void onEditProfileClick() throws IOException {
-        Stage stage = (Stage) editProfileButton.getScene().getWindow();
+        Stage stage = (Stage)this.editProfileButton.getScene().getWindow();
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("edit-profile-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
+        Scene scene = new Scene((Parent)fxmlLoader.load(), 300.0, 450.0);
         stage.setScene(scene);
     }
-
 }
