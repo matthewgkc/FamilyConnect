297699664ae6cb486916ce76bdd6b8e950d1d964
bearfaceington <georgioangelo@gmail.com>
2024-04-28 21:56:47 +1000

    added back button in individual overview page to return to user profile page

--- src/main/java/com/example/familyconnect/OverviewController.java
+++ src/main/java/com/example/familyconnect/OverviewController.java
@@ -6,12 +6,27 @@
 package com.example.familyconnect;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class OverviewController {
     @FXML
     private ChoiceBox<String> trackingPeriodChoiceBox;
+    @FXML
+    private Button backButton;
 
+    @FXML
+    protected void backButtonClick() throws IOException {
+        Stage stage = (Stage) backButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("user-profile-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
+        stage.setScene(scene);
+    }
     public OverviewController() {
     }
 }
