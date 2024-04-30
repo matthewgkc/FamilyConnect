8a64bb920d53373f6581120a8e48a78b35f94c60
yanzahs <lovishaznay@gmail.com>
2024-04-30 12:13:10 +1000

    manage-family

--- src/main/java/com/example/familyconnect/ManageFamilyPageController.java
+++ src/main/java/com/example/familyconnect/ManageFamilyPageController.java
@@ -1,14 +1,28 @@
 package com.example.familyconnect;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ManageFamilyPageController {
 
     @FXML
     private TextField emailOrPhoneNumberField;
+    @FXML
+    private Button backHomeButton;
 
+    @FXML
+    protected void backtoHome() throws IOException {
+        Stage stage = (Stage) backHomeButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
+        stage.setScene(scene);
+    }
     @FXML
     protected void backClick() {
         // Code to go back to previous page
@@ -23,4 +37,5 @@ public class ManageFamilyPageController {
         // Add logic to handle adding family member
         System.out.println("Adding family member with email or phone number: " + emailOrPhoneNumber);
     }
+
 }
