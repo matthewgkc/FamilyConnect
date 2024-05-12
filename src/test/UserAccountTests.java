4c11713bb9dc4e4afbb96ef1d5a3e293f581aab1
matthew <matthewgkcollis@gmail.com>
2024-05-12 18:22:49 +1000

    Created User Account database unit tests

---
+++ src/test/UserAccountTests.java
@@ -0,0 +1,39 @@
+import com.example.familyconnect.model.UserAccount;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserAccountTests {
+
+    private UserAccount account;
+
+    @BeforeEach
+    void setUp() {
+        account = new UserAccount(50,"John Smith", "js");
+    }
+    @Test
+    public void testGetId() {
+        account.setId(100);
+        assertEquals(100, account.getId());
+    }
+    @Test
+    public void testGetUsername() {
+        assertEquals("John Smith", account.getUserName());
+    }
+    @Test
+    public void testSetFirstName() {
+        account.setUsername("Jane Smith");
+        assertEquals("Jane Smith", account.getUserName());
+    }
+    @Test
+    public void testGetPassword() {
+        assertEquals("js", account.getPassword());
+    }
+    @Test
+    public void testSetPassword() {
+        account.setPassword("JSmithy");
+        assertEquals("JSmithy", account.getPassword());
+    }
+
+}
