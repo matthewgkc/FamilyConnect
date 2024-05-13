83466312ab8edd6ff5fb9036eb35844f30273ead
matthew <matthewgkcollis@gmail.com>
2024-05-13 10:15:31 +1000

    Changed name of UserAccountTests to UserAccountTest

--- src/test/java/UserAccountTest.java
+++ src/test/java/UserAccountTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class UserAccountTests {
+public class UserAccountTest {
 
     private UserAccount account;
 
