473f55a7e418f82e7f2519d47f0ea9f990208e0e
matthew <matthewgkcollis@gmail.com>
2024-05-13 23:37:26 +1000

    Changed default view to start view

--- src/main/java/com/example/familyconnect/HelloApplication.java
+++ src/main/java/com/example/familyconnect/HelloApplication.java
@@ -34,7 +34,7 @@ public class HelloApplication extends Application {
      */
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("start.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), WIDTH, HEIGHT);
         String stylesheet = HelloApplication.class.getResource("Home-page-style.css").toExternalForm();
         scene.getStylesheets().add(stylesheet);
